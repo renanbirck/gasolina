84700946f8369df52dd9800d7771e9de016b058b
Renan Birck Pinheiro <renan.birck.pinheiro@gmail.com>
2023-10-28 16:29:34 -0300

    Está raspando os links corretamente, encontrando os PDF.

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -7,7 +7,20 @@ from bs4 import BeautifulSoup
 goal_URL = lambda year: f"https://www.joinville.sc.gov.br/publicacoes/pesquisas-de-precos-combustiveis-{year}"
 
 def get_content_to_URL(url: str):
+    """ Puxa o conteúdo da URL informada. """
+
     logging.info(f'Acessando a URL: {url}')
     url_data = requests.get(url)
     logging.info(f'Status HTTP: {url_data.status_code}')
     return url_data
+
+def get_PDFs_of_URL(url: str):
+    links = []
+    soup = BeautifulSoup(get_content_to_URL(url).content, "html5lib")
+    for link in soup.find_all('a', href=True):
+        if link['href'].endswith('.pdf'):
+            logging.info(f"Encontramos um link para um PDF! {link['href']}")
+            links.append(link['href'])
+    logging.info(f"Encontrei os links: {links}")
+    return links
+
--- scraper/test_scraping.py
+++ scraper/test_scraping.py
@@ -5,10 +5,18 @@ sys.path.append('..')
 import scraper 
 
 class TestScrap(unittest.TestCase):
+    result = None
+
     def test_can_get_to_URL(self):
         """ Verificar que conseguimos chegar no site da prefeitura e obter a página onde estão os links para os PDF. """
-        result = scraper.get_content_to_URL(scraper.goal_URL('2023'))
-        self.assertEqual(result.status_code, 200)
-         
+        self.result = scraper.get_content_to_URL(scraper.goal_URL('2023'))
+        self.assertEqual(self.result.status_code, 200)
+    
+    def test_can_get_PDF_links(self):
+        """ Verificar que conseguimos pegar os links do site anteriormente baixado. """
+        self.result = scraper.get_PDFs_of_URL(scraper.goal_URL('2023'))
+        for link in self.result:
+            self.assertEqual(link.endswith('.pdf'), True)
+
 if __name__ == '__main__':
     unittest.main()
