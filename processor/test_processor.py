da7046e643b239ca5866fed7938ce9b864f46389
Renan Birck Pinheiro <renan.birck.pinheiro@gmail.com>
2023-11-18 17:08:26 -0300

    corrigindo a falta do self

--- processor/test_processor.py
+++ processor/test_processor.py
@@ -5,13 +5,19 @@ import processor_core
 
 class TestProcessor(unittest.TestCase):
     processor = None
+
     def test_can_get_PDF(self):
         target = 'references/Novembro-2023.pdf'
-        processor = processor_core.PDFProcessor(target)
+        self.processor = processor_core.PDFProcessor(target)
 
         # Se tivermos conseguido ler o PDF, alguma coisa vai estar em 'pages'.
-        self.assertNotEqual(processor.pages, None)
+        self.assertNotEqual(self.processor.pages, None)
 
         # Se um dia começar a falhar aqui... talvez o PDF não tenha mais 4 páginas,
         # porque foram adicionados novos postos.
-        self.assertEqual(len(processor.pages), 4)
+        self.assertEqual(len(self.processor.pages), 4)
+
+    def test_can_get_survey(self):
+        # Testar se a gente está na pesquisa certa
+        pass
+
