d7c926f74a52603ec6eae37ee8aeba0b5de5612c
Renan Birck Pinheiro <renan.birck.pinheiro@gmail.com>
2023-11-27 18:56:21 -0300

    Conseguimos criar a estrutura das tabelas, agora é preciso validar a inserção dos dados nelas.

--- processor/database.py
+++ processor/database.py
@@ -19,3 +19,42 @@ class Database:
         logging.info(f"Criando o BD com o nome {self.file_name}.")
         self.connection = sqlite3.connect(self.file_name)
         self.cursor = self.connection.cursor()
+        self.create_tables()
+
+    def create_tables(self):
+        self.cursor.execute("CREATE TABLE IF NOT EXISTS Distribuidoras(\
+                             IdDistribuidora INT PRIMARY KEY,\
+                             NomeDistribuidora VARCHAR(24) NOT NULL\
+                            );")
+        self.cursor.execute("CREATE TABLE IF NOT EXISTS PostosGasolina(\
+                                IdPosto INT PRIMARY KEY,\
+                                IdDistribuidora INT NOT NULL,\
+                                NomePosto VARCHAR(255) NOT NULL,\
+                                EnderecoPosto VARCHAR(255) NOT NULL,\
+                                BairroPosto VARCHAR(32) NOT NULL,\
+                                FOREIGN KEY (IdDistribuidora)\
+                                    REFERENCES Distribuidoras(IdDistribuidora)\
+                                    ON DELETE RESTRICT\
+                             );")
+        self.cursor.execute("CREATE TABLE IF NOT EXISTS Pesquisas(\
+                                IdPesquisa INT PRIMARY KEY,\
+                                DataPesquisa VARCHAR(24) NOT NULL);")
+        self.cursor.execute("CREATE TABLE IF NOT EXISTS Precos(\
+                                IdPreco INT PRIMARY KEY,\
+                                IdPesquisa INT,\
+                                IdPosto INT,\
+                                PrecoGasolinaComum INTEGER,\
+                                PrecoGasolinaAditivada INTEGER,\
+                                PrecoEtanol INTEGER,\
+                                PrecoDiesel INTEGER,\
+                                FOREIGN KEY (IdPesquisa)\
+                                    REFERENCES Pesquisas(IdPesquisa)\
+                                    ON DELETE RESTRICT,\
+                                FOREIGN KEY (IdPosto)\
+                                    REFERENCES Posto(IdPosto)\
+                                    ON DELETE RESTRICT);")
+
+
+        self.connection.commit()
+
+    
--- processor/test_database.py
+++ processor/test_database.py
@@ -13,8 +13,18 @@ class TestDatabase(unittest.TestCase):
         """ Verificar se o DB pode ser criado. """
 
         test_DB = database.Database('pesquisas_test.db')
-
         self.assertEqual(isfile('pesquisas_test.db'), True)
+
+    def test_do_tables_exist(self):
+        """ Verificar se o DB foi corretamente inicializado, isto é, 
+        se ele está persistindo (o D do ACID) a partir do teste anterior. """
+
+        test_DB = database.Database('pesquisas_test.db')
+        for table_name in ['Distribuidoras', 'PostosGasolina', 'Pesquisas', 'Precos']:
+            # sqlite_master é onde estão os dados das tabelas.
+            # Veja: https://stackoverflow.com/questions/1601151/
+            result = test_DB.cursor.execute(f"SELECT name FROM sqlite_master WHERE name='{table_name}'").fetchone()
+            self.assertIsNotNone(result)
 
     def test_adiciona_distribuidoras(self):
         pass
