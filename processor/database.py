5b943fad0e04b9d84a4c168e44688c140f8e9640
Renan Birck Pinheiro <renan.birck.pinheiro@gmail.com>
2023-11-27 18:28:50 -0300

    conseguimos acessar o DB.

--- processor/database.py
+++ processor/database.py
@@ -2,11 +2,20 @@
 
 # Este módulo trata das operações com o banco de dados.
 
-import sqlite3
+import sqlite3, logging
 
 class Database:
     connection, cursor = None, None
+    file_name = None
 
-    def initialize_DB(self, file_name='pesquisas.db'):
-        self.connection = sqlite3.connect(file_name)
+    def __init__(self, file_name='pesquisas.db'):
+        self.file_name = file_name
+        self.initialize_DB()
+
+    def __del__(self):
+        self.connection.commit()
+
+    def initialize_DB(self):
+        logging.info(f"Criando o BD com o nome {self.file_name}.")
+        self.connection = sqlite3.connect(self.file_name)
         self.cursor = self.connection.cursor()
--- processor/test_database.py
+++ processor/test_database.py
@@ -12,14 +12,8 @@ class TestDatabase(unittest.TestCase):
     def test_can_create_DB(self):
         """ Verificar se o DB pode ser criado. """
 
-        test_DB = database.Database()
+        test_DB = database.Database('pesquisas_test.db')
 
-        try:
-            remove('pesquisas_test.db')
-        except:
-            pass
-
-        test_DB.initialize_DB('pesquisas_test.db')
         self.assertEqual(isfile('pesquisas_test.db'), True)
 
     def test_adiciona_distribuidoras(self):
