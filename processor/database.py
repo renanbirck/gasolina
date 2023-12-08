6982606d0c57d42f2602398706e0efe6aeb85f6a
Renan Birck Pinheiro <renan.birck.pinheiro@gmail.com>
2023-12-08 18:51:03 -0300

    1. Corrigi um problema com a chave primária não aumentando sozinha. 2. A coluna do endereço do posto é UNIQUE, pelo que é fisicamente impossível ter dois postos de gasolina diferentes no mesmo endereço.

--- processor/database.py
+++ processor/database.py
@@ -23,26 +23,26 @@ class Database:
 
     def create_tables(self):
         self.cursor.execute("CREATE TABLE IF NOT EXISTS Distribuidoras(\
-                             IdDistribuidora INT PRIMARY KEY,\
+                             IdDistribuidora INTEGER PRIMARY KEY NOT NULL,\
                              NomeDistribuidora VARCHAR(24) UNIQUE NOT NULL\
                             );")
         self.cursor.execute("CREATE TABLE IF NOT EXISTS PostosGasolina(\
-                                IdPosto INT PRIMARY KEY,\
-                                IdDistribuidora INT NOT NULL,\
+                                IdPosto INTEGER PRIMARY KEY NOT NULL,\
+                                IdDistribuidora INTEGER NOT NULL,\
                                 NomePosto VARCHAR(255) NOT NULL,\
-                                EnderecoPosto VARCHAR(255) NOT NULL,\
+                                EnderecoPosto VARCHAR(255) UNIQUE NOT NULL,\
                                 BairroPosto VARCHAR(32) NOT NULL,\
                                 FOREIGN KEY (IdDistribuidora)\
                                     REFERENCES Distribuidoras(IdDistribuidora)\
                                     ON DELETE RESTRICT\
                              );")
         self.cursor.execute("CREATE TABLE IF NOT EXISTS Pesquisas(\
-                                IdPesquisa INT PRIMARY KEY,\
+                                IdPesquisa INTEGER PRIMARY KEY NOT NULL,\
                                 DataPesquisa VARCHAR(24) NOT NULL);")
         self.cursor.execute("CREATE TABLE IF NOT EXISTS Precos(\
-                                IdPreco INT PRIMARY KEY,\
-                                IdPesquisa INT,\
-                                IdPosto INT,\
+                                IdPreco INTEGER PRIMARY KEY NOT NULL,\
+                                IdPesquisa INTEGER,\
+                                IdPosto INTEGER,\
                                 PrecoGasolinaComum INTEGER,\
                                 PrecoGasolinaAditivada INTEGER,\
                                 PrecoEtanol INTEGER,\
