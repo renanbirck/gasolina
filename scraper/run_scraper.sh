e45712bba292aa69029cfa51b657baaf490e8d91
Renan Birck Pinheiro <renan.birck.pinheiro@gmail.com>
2024-02-23 22:14:51 -0300

    Agora o script do scraper permite especificar um ano, ou uma faixa de anos.

--- scraper/run_scraper.sh
+++ scraper/run_scraper.sh
@@ -1,10 +1,22 @@
 #!/usr/bin/env bash
 
+run_scraper () {
+	echo "Chamando o scraper para o ano de $1."
+	./scraper.py "$1"
+}
+
 if [ $# -eq 0 ]
 then
-	echo "Forneça um ano para baixar os PDFs!"
+	echo "Forneça um ano para baixar os PDFs, ou uma sequência de anos."
 	exit 1
+elif [ $# -eq 1 ]
+then 
+	run_scraper $1
+elif [ $# -eq 2 ] 
+then 
+	for YEAR in $(seq $1 $2)
+	do
+		run_scraper $YEAR
+	done
 fi
 
-echo "Chamando o scraper para o ano de $@."
-./scraper.py "$@"
