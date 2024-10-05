72dd952a8e3b71eeeedc5fda7a37dc04fa94f1c1
Renan Birck Pinheiro <renan.birck.pinheiro@gmail.com>
2024-10-05 14:12:19 -0300

    começando a escrever o código para o acesso ao banco de dados.

---
+++ api/database.py
@@ -0,0 +1,20 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+## database.py: arquivo que trata da conexão ao BD com os dados das pesquisas.
+## O modelo de dados, com as tabelas e relacionamentos, é descrito em model.py
+
+DB_URL = "sqlite:///../parser/pesquisas.db" # Caminho para o BD
+
+# Criar a conexão ao BD
+
+engine = create_engine(
+    DB_URL, connect_args={"check_same_thread": False}
+)
+
+Base = declarative_base()
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+# Até aqui, é quase tudo igual à documentação. 
