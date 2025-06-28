e7d26182c06f6af5fb34801d67d805604144775e
Renan Birck Pinheiro <renan.birck.pinheiro@gmail.com>
2025-06-28 17:13:43 -0300

    fix(tests): o teste da pesquisa passou 	testando se a pesquisa foi realmente adicionada ao BD

--- tests/test_api.py
+++ tests/test_api.py
@@ -4,7 +4,7 @@ from datetime import datetime
 class TestAPI(unittest.TestCase):
 
     target_URL = 'http://localhost:8000'
-
+    today = str(datetime.now().strftime("%Y%m%d"))
 
     def test_estamos_em_dev(self):
         # Estamos no ambiente de desenvolvimento?
@@ -19,23 +19,25 @@ class TestAPI(unittest.TestCase):
 
     def test_criar_pesquisa(self):
         # então testar especificar com uma data
-        today = str(datetime.now().strftime("%Y%m%d"))
-        request_data = requests.post(f'{self.target_URL}/pesquisa/nova', json = {'data': today})
 
+        request_data = requests.post(f'{self.target_URL}/pesquisa/nova', json = {'data': self.today})
         json_output = request_data.json()
+
         self.assertEqual(request_data.status_code, 200)
         self.assertEqual(json_output['id'], 1)
-        self.assertEqual(json_output['data'], today)
+        self.assertEqual(json_output['data'], self.today)
 
     def test_ja_existe_pesquisa(self):
-
         # então testar especificar com uma data
-        today = str(datetime.now().strftime("%Y%m%d"))
-        request_data = requests.post(f'{self.target_URL}/pesquisa/nova', json = {'data': today})
 
+        request_data = requests.post(f'{self.target_URL}/pesquisa/nova', json = {'data': self.today})
         json_output = request_data.json()
         self.assertEqual(request_data.status_code, 422)
 
+    def test_pegar_pesquisa(self):
+        request_data = requests.get(f'{self.target_URL}/pesquisas').json()
+        self.assertEqual(request_data[0]["id"], 1)
+        self.assertEqual(request_data[0]["data"], self.today)
 
 if __name__ == '__main__':
     unittest.main()
