fcc5dd371680ae3af51f8c9597f62777b7c1ad6d
Renan Birck Pinheiro <renan.birck.pinheiro@gmail.com>
2025-06-28 12:18:11 -0300

    chore(test): Começar a testar a API
    	Escrever alguns testes para a API.

---
+++ config.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Arquivo de configuração, para evitar que precisemos repetir constantes e nomes de arquivos.
+# 
+#
+
+DB_FILE = '~/Sources/gasolina/data/precos.db'  # DB onde são armazenados os dados.
+PDF_PATH = '~/Sources/gasolina/data'           # Onde serão armazenados os PDFs coletados.
+
+INITIAL_YEAR, FINAL_YEAR = 2021, 2024          # A série histórica da prefeitura de Joinville
+                                               # começa em 2021, e ainda assim é incompleta.
+
+
