19885a3023b583c6ea3a740625bc10bd4693d080
Renan Birck Pinheiro <renan.birck.pinheiro@gmail.com>
2025-06-28 12:44:53 -0300

    feat(api): uma forma de sabermos se estamos em ambiente de dev ou de produção
    	Para desenvolvimento, usamos um BD limpo para não alterar o BD principal.
    	Setando a variável de ambiente DB_PATH, é possível especificar onde fica esse BD
    	e, se ela não estiver setada, usa o BD de produção.

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,7 +1,20 @@
 import unittest, requests
+import signal
 
 class TestAPI(unittest.TestCase):
-    pass
+
+    target_URL = 'http://localhost:8000'
+
+    def test_chegar_servidor(self):
+        # Testar se a gente consegue chegar no servidor.
+        requests.get(f'{self.target_URL}/')
+
+    def test_criar_pesquisa(self):
+        pass
+
+    def test_ja_existe_pesquisa(self):
+        pass
+
 
 if __name__ == '__main__':
     unittest.main()
