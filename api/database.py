a5c118664acc8f71dde119c849d02ce1d4dec770
Renan Birck Pinheiro <renan.birck.pinheiro@gmail.com>
2025-06-28 11:50:22 -0300

    feat(database): permitir configurar um caminho para o BD
    	Passando a variável DB_PATH, é possível escolher qual arquivo
    	de BD será utilizado. Isso irá simplificar os testes, porque
    	fica mais fácil começar de um BD em branco.

--- api/database.py
+++ api/database.py
@@ -1,13 +1,26 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from os import environ
+import logging
 
 ## database.py: arquivo que trata da conexão ao BD com os dados das pesquisas.
 ## O modelo de dados, com as tabelas e relacionamentos, é descrito em model.py
 
-DB_URL = "sqlite:///../data/pesquisas.db" # Caminho para o BD
+logging.basicConfig(level=logging.INFO) # O nível de logging é INFO.
+
+
+# Permitir que a variável de ambiente DB_PATH seja usada para definir onde o DB está salvo.
+# Vai facilitar os testes posteriormente.
+
+try:
+    DB_URL = f"sqlite:///{environ["DB_PATH"]}"
+except: # Se não especificar, é o default
+    DB_URL = "sqlite:///../data/pesquisas.db" # Caminho para o BD
 
 # Criar a conexão ao BD
+
+logging.info(f"O caminho do BD é {DB_URL}.")
 
 engine = create_engine(
     DB_URL, connect_args={"check_same_thread": False}
