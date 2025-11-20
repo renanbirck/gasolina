b75c4e861f1da17a703b71131c50b00c0b87b52e
Renan Birck Pinheiro <renan.birck.pinheiro@gmail.com>
2025-11-20 17:11:11 -0300

    chore(database): script para carga manual funcionou! 	Estamos conseguindo incluir no banco de dados manualmente.

---
+++ misc/manual_load.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+import csv
+import sqlite3
+
+# Este script carrega dados manualmente na pesquisa mais recente,
+# presumindo que ela já tenha sido criada.
+
+CSV_SOURCE = "../parser/result.csv"
+DATABASE_PATH = "../data/pesquisas.db"
+
+connection = sqlite3.connect(DATABASE_PATH)
+cursor = connection.cursor()
+
+cursor.execute("SELECT IdPesquisa FROM Pesquisas ORDER BY IdPesquisa DESC LIMIT 1")
+id_pesquisa = cursor.fetchone()[0]
+processed_rows = []
+
+print(f"O ID da pesquisa mais atual é {id_pesquisa}.")
+
+with open(CSV_SOURCE, "r") as csvfile:
+    data = csv.reader(csvfile)
+
+    for row in data:  # Converter os tipos de dados e encaixar no formato da query.
+        new_row = [id_pesquisa]
+
+        new_row.append(int(row[0]))
+        for entry in map(lambda x: float(x) if x else None, row[1:]):
+            new_row.append(entry)
+
+        processed_rows.append(new_row)
+    print(processed_rows)
+
+# OK, então fazer a inserção no BD!
+#
+#
+cursor.executemany(
+    "INSERT INTO Precos(IdPesquisa, \
+    IdPosto, \
+    PrecoGasolinaComum, \
+    PrecoGasolinaAditivada, \
+    PrecoEtanol, \
+    PrecoDiesel, \
+    PrecoGNV, \
+    PrecoGasolinaPremium) VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
+    processed_rows,
+)
+connection.commit()
